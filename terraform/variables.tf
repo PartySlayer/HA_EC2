f5f48d140ef8c0c8ecc189097f5fad6f73ace0b7
PartySlayer <squadrittichristian@gmail.com>
2024-03-09 13:19:28 +0100

    Initialized a modular approach

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS region"
+}
+
+variable "aws_access_key" {
+  description = "AWS access key"
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+}
